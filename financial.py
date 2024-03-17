366cfc3dedd367f8e428dd068b9fa87ec4c3aaf6
Hepnerd <20031350+Hepnerd@users.noreply.github.com>
2024-03-16 21:02:40 -0400

    Update financial.py

--- financial.py
+++ financial.py
@@ -19,120 +19,104 @@ def read_bank_csv_file(file_path):
             data.append(row)
     return data
 
-def remove_duplicates(data):
-    """
-    Removes duplicate rows from the data list.
-    Assumes that each row is represented as a dictionary.
-    """
-    seen = set()
-    unique_data = []
-    for row in data:
-        row_tuple = tuple(row.items())
-        if row_tuple not in seen:
-            seen.add(row_tuple)
-            unique_data.append(row)
-    return unique_data
-
 def remove_duplicates_from_final(output_filename):
+    location = "output/"
     toclean = pd.read_csv(output_filename)
     deduped = toclean.drop_duplicates(['Date','Transaction','Description','transactionType'])
-    deduped.to_csv(output_filename + "final.csv")
-    os.remove(output_filename)
+    deduped.to_csv(location + "transactions.csv", index=False)
 
-def process_bank_csv_files(directory_path, output_filename):
+def process_bank_csv_files(output_filename):
     """
     Processes all CSV files in the specified directory.
     Assumes that each file ends with 'transactions.csv'.
     """
-    for filename in os.listdir(directory_path):
-        if filename.lower().endswith('transactions.csv'):
-            file_path = os.path.join(directory_path, filename)
-            data = read_bank_csv_file(file_path)
-            transactionArray = []
-            
-            # Manually assign fields based on file-specific logic
-            for row in data:
-                transactionDate = ""
-                description = ""
-                transaction = 0
-                category = ""
-                bank = ""
-                transactionType = ""
-                # Example: Assigning 'amount' field based on file name
-                if 'pnc' in filename.lower():
-                    if row['Date'] != '':
-                        date = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').strftime('%m/%d/%Y')
-                    else:
-                        date = ''
-                    transactionDate = date
-                    description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
-                    transaction =+ abs(float(row['Withdrawals'].replace("$", "").replace(",", "") or 0))
-                    transaction += abs(float(row['Deposits'].replace("$", "").replace(",", "") or 0))
-                    if row['Withdrawals'] == '':
-                        transactionType = 'debit'
-                    else:
-                        transactionType = 'credit'
-                    category = row['Category']
-                    bank = 'PNC'
-                elif 'key' in filename.lower():
-                    if row['Date'] != '' and "/" in row['Date'] and row['Date'] != 'Date':
-                        date = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').strftime('%m/%d/%Y')
-                    else:
-                        date = ''
-                    transactionDate = date
-                    match = re.search('^[\d]+', row['Amount'])
-                    if not match:
-                        continue
-                    transaction = abs(float(row['Amount'] or 0))
-                    if float(row['Amount'] or 0) < 0:
-                        transactionType = 'credit'
-                    else:
-                        transactionType = 'debit'
-                    description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
-                    category = row['Category']
-                    bank = 'KEYBANK'
-                elif 'ally' in filename.lower():
-                    if row['Date'] != '':
-                        date = datetime.datetime.strptime(row['Date'], '%Y-%m-%d').strftime('%m/%d/%Y')
-                    else:
-                        date = ''
-                    transactionDate = date
-                    transaction = abs(float(row['Amount']))
-                    category = row['Category']
-                    description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
-                    bank = 'ALLY'
-                    if row['Type'] == 'Withdrawal':
-                        transactionType = 'credit'
-                    elif row['Type'] == 'Deposit':
-                        transactionType = 'debit'
-                elif 'mint' in filename.lower():
-                    if row['Date'] != '':
-                        date = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').strftime('%m/%d/%Y')
-                    else:
-                        date = ''
-                    transactionDate = date
-                    description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
-                    transaction = abs(float(row['Amount']))
-                    transactionType = row['Transaction Type']
-                    category = row['Category']
-                    bank = 'MINT'
+    transactionFile = filedialog.askopenfilename(title="Open CSV File", filetypes=[("CSV files", "*.csv")])
+    filename = os.path.basename(transactionFile)
+    data = read_bank_csv_file(transactionFile)
+    transactionArray = []
+    
+    # Manually assign fields based on file-specific logic
+    for row in data:
+        transactionDate = ""
+        description = ""
+        transaction = 0
+        category = ""
+        bank = ""
+        transactionType = ""
+        # Example: Assigning 'amount' field based on file name
+        if 'pnc' in filename.lower():
+            if row['Date'] != '':
+                date = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').strftime('%m/%d/%Y')
+            else:
+                date = ''
+            transactionDate = date
+            description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
+            transaction =+ abs(float(row['Withdrawals'].replace("$", "").replace(",", "") or 0))
+            transaction += abs(float(row['Deposits'].replace("$", "").replace(",", "") or 0))
+            if row['Withdrawals'] == '':
+                transactionType = 'debit'
+            else:
+                transactionType = 'credit'
+            category = row['Category']
+            bank = 'PNC'
+        elif 'key' in filename.lower():
+            if row['Date'] != '' and "/" in row['Date'] and row['Date'] != 'Date':
+                date = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').strftime('%m/%d/%Y')
+            else:
+                date = ''
+            transactionDate = date
+            match = re.search('^[\d]+', row['Amount'])
+            if not match:
+                continue
+            transaction = abs(float(row['Amount'] or 0))
+            if float(row['Amount'] or 0) < 0:
+                transactionType = 'credit'
+            else:
+                transactionType = 'debit'
+            description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
+            category = row['Category']
+            bank = 'KEYBANK'
+        elif 'ally' in filename.lower():
+            if row['Date'] != '':
+                date = datetime.datetime.strptime(row['Date'], '%Y-%m-%d').strftime('%m/%d/%Y')
+            else:
+                date = ''
+            transactionDate = date
+            transaction = abs(float(row['Amount']))
+            category = row['Category']
+            description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
+            bank = 'ALLY'
+            if row['Type'] == 'Withdrawal':
+                transactionType = 'credit'
+            elif row['Type'] == 'Deposit':
+                transactionType = 'debit'
+        elif 'mint' in filename.lower():
+            if row['Date'] != '':
+                date = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').strftime('%m/%d/%Y')
+            else:
+                date = ''
+            transactionDate = date
+            description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
+            transaction = abs(float(row['Amount']))
+            transactionType = row['Transaction Type']
+            category = row['Category']
+            bank = 'MINT'
 
-                # Add the filename as a prefix to each field
-                # for key in row:
-                #     bank = f"{filename.split('_')[0]}"
-                
-                transactionArray.insert(1, "{0},{1},{2},{3},{4},{5}".format(bank,transactionDate,transaction,description,category,transactionType))
-            # Remove duplicates
-            # unique_data = remove_duplicates(transactionArray)
-            transactionArray = list(set(transactionArray))
-            
-            # Write the processed data to a new CSV file
-            with open(output_filename, 'a+', newline='') as outfile:
-                writer = csv.writer(outfile)
-                for item in transactionArray:
-                    splititem = item.split(",")
-                    writer.writerow([[splititem[0]], [splititem[1]], splititem[2], splititem[3], splititem[4], splititem[5]])
-            print(f"Processed data (without duplicates) written to {output_filename}")
+        # Add the filename as a prefix to each field
+        # for key in row:
+        #     bank = f"{filename.split('_')[0]}"
+        
+        transactionArray.insert(1, "{0},{1},{2},{3},{4},{5}".format(bank,transactionDate,transaction,description,category,transactionType))
+    # Remove duplicates
+    transactionArray = list(set(transactionArray))
+    
+    # Write the processed data to a new CSV file
+    with open(output_filename, 'a+', newline='') as outfile:
+        writer = csv.writer(outfile)
+        for item in transactionArray:
+            splititem = item.split(",")
+            writer.writerow([[splititem[0]], [splititem[1]], splititem[2], splititem[3], splititem[4], splititem[5]])
+    print(f"Processed data (without duplicates) written to {output_filename}")
 
 # Function to sort the Treeview by column
 def sort_treeview(tree, col, descending):
@@ -152,24 +136,36 @@ def transactionFileIntoList(fileName):
         print("Ah dang")
 
 def insert_transactions():
-    directory_to_process = 'input'
     output_filename = "output/transactions.csv"
+    process_bank_csv_files(output_filename)
+    remove_duplicates_from_final(output_filename)
+
+def create_transactions_file_if_not_exist(output_filename):
     fields = ['Bank', 'Date', 'Transaction', 'Description', 'Category', 'transactionType']
     file_exists = os.path.isfile(output_filename)
     if not file_exists:
         with open(output_filename, 'w', newline='') as outfile:
                         writer = csv.writer(outfile)
                         writer.writerow(fields)
-    process_bank_csv_files(directory_to_process, output_filename)
-    remove_duplicates_from_final(output_filename)
 
-TRANSACTION_PATH = "output/transactions.csvfinal.csv"
+def new_iterable_list(transaction_PATH):
+    global transactionList
+    transactionList = iter(transactionFileIntoList(TRANSACTION_PATH))
+
+TRANSACTION_PATH = "output/transactions.csv"
+
+create_transactions_file_if_not_exist(TRANSACTION_PATH)
 
 transactionList = iter(transactionFileIntoList(TRANSACTION_PATH))
 # transactionIterable = iter(transactionList)
 
 root = tk.Tk()
 root.title("Hepnerd Transaction Viewer")
+
+style = ttk.Style(root)
+root.tk.call("source", "forest-light.tcl")
+root.tk.call("source", "forest-dark.tcl")
+style.theme_use("forest-dark")
 
 root.geometry("1600x600")
 
@@ -179,7 +175,7 @@ root.geometry("1600x600")
 insert_data = tk.Button(root, text="Insert transactions", command=insert_transactions)
 insert_data.pack(padx=20, pady=10)
 
-filter_dropdown = tk.Label(root, text="Dropwdown", padx=20, pady=10)
+filter_dropdown = tk.Label(root, text="Filter Dropdown Here", padx=20, pady=10)
 filter_dropdown.pack()
 
 tree = ttk.Treeview(root, show="headings")
@@ -196,11 +192,21 @@ try:
         for col in header:
             tree.heading(col, text=col, command=lambda c=col: sort_treeview(tree, c, False))
             tree.column(col, width=200)
-
+        
         for row in transactionList:
             tree.insert("", "end", values=row)
 
 except Exception as e:
     status_label.config(text=f"Error: {str(e)}")
+
+verscrlbar = ttk.Scrollbar(tree, orient ="vertical", command = tree.yview)
+
+# Calling pack method w.r.to vertical 
+# scrollbar
+verscrlbar.pack(side ='right', fill ='y')
+ 
+# Configuring treeview
+tree.configure(yscrollcommand = verscrlbar.set)
+
 
 root.mainloop()
