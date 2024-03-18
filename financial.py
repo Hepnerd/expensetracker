699274b393cee7d52fbe2128024cdb9e97c48f81
Hepnerd <20031350+Hepnerd@users.noreply.github.com>
2024-03-17 22:08:59 -0400

    Update financial.py

--- financial.py
+++ financial.py
@@ -1,8 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
+from tkinter import *
 import csv
-import sys, os
+import os
 import datetime
 import re
 import pandas as pd
@@ -58,7 +59,7 @@ def process_bank_csv_files(output_filename):
             else:
                 transactionType = 'credit'
             category = row['Category']
-            bank = 'PNC'
+            bank = "PNC"
         elif 'key' in filename.lower():
             if row['Date'] != '' and "/" in row['Date'] and row['Date'] != 'Date':
                 date = datetime.datetime.strptime(row['Date'], '%m/%d/%Y').strftime('%m/%d/%Y')
@@ -75,7 +76,7 @@ def process_bank_csv_files(output_filename):
                 transactionType = 'debit'
             description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
             category = row['Category']
-            bank = 'KEYBANK'
+            bank = "KEYBANK"
         elif 'ally' in filename.lower():
             if row['Date'] != '':
                 date = datetime.datetime.strptime(row['Date'], '%Y-%m-%d').strftime('%m/%d/%Y')
@@ -85,7 +86,7 @@ def process_bank_csv_files(output_filename):
             transaction = abs(float(row['Amount']))
             category = row['Category']
             description = row['Description'].replace(",", "").replace(";", "").replace(":", "")
-            bank = 'ALLY'
+            bank = "ALLY"
             if row['Type'] == 'Withdrawal':
                 transactionType = 'credit'
             elif row['Type'] == 'Deposit':
@@ -100,11 +101,7 @@ def process_bank_csv_files(output_filename):
             transaction = abs(float(row['Amount']))
             transactionType = row['Transaction Type']
             category = row['Category']
-            bank = 'MINT'
-
-        # Add the filename as a prefix to each field
-        # for key in row:
-        #     bank = f"{filename.split('_')[0]}"
+            bank = "MINT"
         
         transactionArray.insert(1, "{0},{1},{2},{3},{4},{5}".format(bank,transactionDate,transaction,description,category,transactionType))
     # Remove duplicates
@@ -116,7 +113,6 @@ def process_bank_csv_files(output_filename):
         for item in transactionArray:
             splititem = item.split(",")
             writer.writerow([[splititem[0]], [splititem[1]], splititem[2], splititem[3], splititem[4], splititem[5]])
-    print(f"Processed data (without duplicates) written to {output_filename}")
 
 # Function to sort the Treeview by column
 def sort_treeview(tree, col, descending):
@@ -148,26 +144,45 @@ def create_transactions_file_if_not_exist(output_filename):
                         writer = csv.writer(outfile)
                         writer.writerow(fields)
 
-def new_iterable_list(transaction_PATH):
-    global transactionList
-    transactionList = iter(transactionFileIntoList(TRANSACTION_PATH))
+def extractFirstFromList(lst):
+    return [item[0] for item in lst]
+
+# def new_iterable_list(TRANSACTION_PATH):
+#     global transactionList
+#     transactionList = iter(transactionFileIntoList(TRANSACTION_PATH))
 
 TRANSACTION_PATH = "output/transactions.csv"
 
 create_transactions_file_if_not_exist(TRANSACTION_PATH)
 
+transactionReference = transactionFileIntoList(TRANSACTION_PATH)
 transactionList = iter(transactionFileIntoList(TRANSACTION_PATH))
 # transactionIterable = iter(transactionList)
 
 root = tk.Tk()
+transactions = ttk.Frame(root)
+transactions.pack()
+
+dashboard = ttk.Frame(root)
+dashboard.pack()
+
+income = ttk.Frame(root)
+income.pack()
+
+transactionsTable = ttk.Frame(transactions)
+transactionsTable.grid(row=0, column=1, padx=10, pady=10)
+
+transactionEdit = ttk.LabelFrame(transactions, text="Edit")
+transactionEdit.grid(row=0, column=0, padx=10)
+
 root.title("Hepnerd Transaction Viewer")
 
-style = ttk.Style(root)
-root.tk.call("source", "forest-light.tcl")
-root.tk.call("source", "forest-dark.tcl")
-style.theme_use("forest-dark")
+# style = ttk.Style(root)
+# root.tk.call("source", "forest-light.tcl")
+# root.tk.call("source", "forest-dark.tcl")
+# style.theme_use("forest-dark")
 
-root.geometry("1600x600")
+root.geometry("2000x1000")
 
 #TODO: 
 # Auto refresh table
@@ -180,41 +195,49 @@ root.geometry("1600x600")
 # Settings saved in CSV
 # Budget tracker
 
-insert_data = tk.Button(root, text="Insert transactions", command=insert_transactions)
-insert_data.pack(padx=20, pady=10)
+print(set(extractFirstFromList(transactionReference)))
 
-filter_dropdown = tk.Label(root, text="Filter Dropdown Here", padx=20, pady=10)
-filter_dropdown.pack()
+combo_list = (list(set(extractFirstFromList(transactionReference))))
 
-tree = ttk.Treeview(root, show="headings")
-tree.pack(padx=20, pady=20, fill="both", expand=True)
+while ("Bank" in combo_list):
+    combo_list.remove("Bank")
 
-status_label = tk.Label(root, text="", padx=20, pady=10)
-status_label.pack()
+status_combobox = ttk.Combobox(transactionEdit, values=combo_list)
 
-try:
-        header = next(transactionList)  # Read the header row
-        tree.delete(*tree.get_children())  # Clear the current data
+file_exists = os.path.isfile(TRANSACTION_PATH)
+if not file_exists:
+    status_combobox.current(0)
+status_combobox.grid(row=0, column=0, padx=5, pady=5,  sticky="ew")
 
-        tree["columns"] = header
-        for col in header:
-            tree.heading(col, text=col, command=lambda c=col: sort_treeview(tree, c, False))
-            tree.column(col, width=200)
-        
-        for row in transactionList:
-            tree.insert("", "end", values=row)
+insert_data = tk.Button(transactionsTable, text="Insert transactions", command=insert_transactions)
+insert_data.grid(row=0, column=0, padx=20, pady=10)
 
-except Exception as e:
-    status_label.config(text=f"Error: {str(e)}")
+filter_dropdown = tk.Label(transactionsTable, text="Filter Dropdown Here", padx=20, pady=10)
+filter_dropdown.grid(row=1, column=0, padx=20, pady=10)
 
-verscrlbar = ttk.Scrollbar(tree, orient ="vertical", command = tree.yview)
+treeFrame = ttk.Frame(transactionsTable)
+treeFrame.grid(row=2, column=0, padx=20, pady=20)
 
-# Calling pack method w.r.to vertical 
-# scrollbar
-verscrlbar.pack(side ='right', fill ='y')
+# treeScroll = ttk.Scrollbar(treeFrame, orient ="vertical", command = tree.yview)
+treeScroll = ttk.Scrollbar(treeFrame, orient ="vertical")
+treeScroll.pack(side ='right', fill ='y')
+
+tree = ttk.Treeview(treeFrame, show="headings", height=30, yscrollcommand=treeScroll.set)
+tree.pack(fill="both", expand=True)
+
+header = next(transactionList)  # Read the header row
+tree.delete(*tree.get_children())  # Clear the current data
+
+tree["columns"] = header
+for col in header:
+    tree.heading(col, text=col, command=lambda c=col: sort_treeview(tree, c, False))
+    tree.column(col, width=200)
+
+for row in transactionList:
+    tree.insert("", "end", values=row)
+
+treeScroll.config(command=tree.yview)
  
-# Configuring treeview
-tree.configure(yscrollcommand = verscrlbar.set)
-
+tree.configure(yscrollcommand = treeScroll.set)
 
 root.mainloop()
